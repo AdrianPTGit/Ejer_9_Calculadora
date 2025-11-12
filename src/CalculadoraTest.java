88e873080cb28fafbef3dd869018807f60e31e6b
Adrian Pomareta <adrianpomaretagit@gmail.com>
2025-11-12 08:50:58 +0100

    Creado java-21.yml

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -3,7 +3,7 @@ public class CalculadoraTest {
         Calculadora calc = new Calculadora();
 
         // Pruebas con assert
-        assert calc.suma(2, 3) == 5 : "Error en suma";
+        assert calc.suma(2, 3) == 5 : "Error en suma"; //assert llam al metodo suma() y comprueba el resultado del método.
         assert calc.resta(5, 2) == 3 : "Error en resta";
 
         System.out.println("Todos los tests pasaron correctamente.");
